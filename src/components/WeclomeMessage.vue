ac51d1fa139a645fc0ff5039fb8d5af7bdfc3823
Mahmoud_Fady <58427542+MahmoudFady@users.noreply.github.com>
2021-02-18 21:17:29 +0200

    first code in vue

---
+++ src/components/WeclomeMessage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>welcome to our app</h1>
+        <p>im mahmoud fady amin</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "WelcomeMessage"
+};
+</script>
+<style lang="scss">
+$mainColor: #f00;
+h1 {
+    color: $mainColor;
+}
+</style>
